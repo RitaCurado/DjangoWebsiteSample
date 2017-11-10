2efab377ebd4ad21e9920c7ff8993766dcc3e39a
RitaCurado <curado.rita@gmail.com>
2017-11-10 18:58:35 +0000

    HomePage concluded Classes added

--- shop/models.py
+++ shop/models.py
@@ -4,3 +4,18 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(max_length=500)
+    price = models.FloatField()
+    image = models.ImageField()
+    manufacturer = models.ForeignKey('Manufacturer', on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(max_length=500)
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
